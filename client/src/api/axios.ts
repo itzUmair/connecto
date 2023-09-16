b18938e3e8ff17c4560bdecf442d84311bf61e59
umair <muhammadumair2511@gmail.com>
2023-09-16 19:10:14 +0500

    fixed api url

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://connecto-api.vercel.app/",
+  baseURL: "https://connecto-api.vercel.app/api/v1",
   withCredentials: true,
 });
